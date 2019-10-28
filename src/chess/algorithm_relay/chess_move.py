36b078db47c395640ab0e040a55c7b3fdf4a2ed8
gsrmeen <patryk.gsrmeen@gmail.com>
2019-10-28 18:51:48 +0100

    Add template chess connector classes

---
+++ src/chess/algorithm_relay/chess_move.py
@@ -0,0 +1,6 @@
+from src.uct.game.base_game_move import BaseGameMove
+
+
+class ChessMove(BaseGameMove):
+    # TODO: Grzesiek
+    pass
---
+++ src/chess/algorithm_relay/chess_state.py
@@ -0,0 +1,15 @@
+from src.uct.game.base_game_state import BaseGameState
+
+
+class ChessState(BaseGameState):
+    def get_all_possible_moves(self):
+        pass
+
+    def perform_random_move(self):
+        pass
+
+    def deep_copy(self):
+        pass
+
+    def apply_moves(self, moves):
+        pass
