eb029865ac32a81f658f1c2c14ccc4333834e396
gsrmeen <patryk.gsrmeen@gmail.com>
2019-11-03 00:35:17 +0100

    Add plotting edges

--- src/visualisation/_main_visualisation.py
+++ src/visualisation/_main_visualisation.py
@@ -8,11 +8,14 @@ y = []
 
 
 def display_tree(tree: Walkers.DrawTree):
+    x_parent = tree.x
+    y_parent = 6 - tree.y
     x.append(tree.x)
     y.append(6 - tree.y)
     print(tree)
     for child in tree.children:
         display_tree(child)
+        plt.plot([x_parent, child.x], [y_parent, 6- child.y], "ro-")
 
 
 example_tree = Tree.get_tree()
