355a28f5152fb77ac2f376a7b67194825b7d8cf8
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2019-11-08 20:16:27 +0100

    CHange python array to numpy

--- src/utils/array_utils.py
+++ src/utils/array_utils.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 def get_2d_array_deep_copy(ref_array):
-    return [[x for x in row] for row in ref_array]
+    return ref_array.copy()
 
 
 def generate_2d_zeros_array(width, height):
-    return [[0] * width] * height
+    return np.zeros(shape=(width, height), dtype=int)
 
 
 def generate_2d_nones_array(width, height):
-    return [[None] * width for i1 in range(height)]
+    return np.full((width, height), None)
