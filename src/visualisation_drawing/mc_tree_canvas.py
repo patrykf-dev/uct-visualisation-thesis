6fe8ec2d4d7b166041072466d711cd0bab74d181
gsrmeen <patryk.gsrmeen@gmail.com>
2019-11-10 23:34:30 +0100

    Make canvas responsible for handling events

--- src/visualisation_drawing/mc_tree_canvas.py
+++ src/visualisation_drawing/mc_tree_canvas.py
@@ -8,15 +8,19 @@ from src.visualisation_drawing.shaders.shader_reader import ShaderReader
 
 
 class MonteCarloTreeCanvas(VispyApp.Canvas):
+    KEY_CODE_LEFT = 16777234
+    KEY_CODE_UP = 16777235
+    KEY_CODE_RIGHT = 16777236
+    KEY_CODE_DOWN = 16777237
+
     def __init__(self, tree, **kwargs):
         VispyApp.Canvas.__init__(self, size=(1000, 1000), **kwargs)
 
+        self.mouse_tics = 0
+        self._setup_widget()
         self._bind_buffers(tree)
         self._bind_shaders()
         self._setup_matrices()
-
-        set_viewport(0, 0, *self.physical_size)
-        set_state(clear_color='gray', depth_test=False, blend=True, blend_func=('src_alpha', 'one_minus_src_alpha'))
 
     def on_resize(self, event):
         set_viewport(0, 0, *event.physical_size)
@@ -26,6 +30,17 @@ class MonteCarloTreeCanvas(VispyApp.Canvas):
         self.program_edges.draw('lines', self.edges_buffer)
         self.program_vertices.draw('points')
 
+    def handle_key_press_event(self, event):
+        print(f"Key pressed canvas: {event.key()}")
+
+    def handle_wheel_event(self, event):
+        self.mouse_tics = self.mouse_tics + event.angleDelta().y() / 120
+        if self.mouse_tics > 30:
+            self.mouse_tics = 30
+        elif self.mouse_tics < -30:
+            self.mouse_tics = -30
+        self.react_to_mouse_scroll(self.mouse_tics)
+
     def react_to_mouse_scroll(self, mouse_tics):
         if mouse_tics == 0:
             scale = 1
@@ -33,8 +48,6 @@ class MonteCarloTreeCanvas(VispyApp.Canvas):
             scale = 1 + (mouse_tics / 30)
         else:
             scale = 1 + (mouse_tics / 30)
-
-        print(f"I GOT {mouse_tics} tics, scaling to: {scale}")
         self._scale_view(scale)
         self.update()
 
@@ -73,3 +86,10 @@ class MonteCarloTreeCanvas(VispyApp.Canvas):
         self.program_edges['u_view'][0] = scale
         self.program_edges['u_view'][5] = scale
         self.program_edges['u_view'][10] = scale
+
+    def _setup_widget(self):
+        self.native.keyPressEvent = self.handle_key_press_event
+        self.native.wheelEvent = self.handle_wheel_event
+
+        set_viewport(0, 0, *self.physical_size)
+        set_state(clear_color='gray', depth_test=False, blend=True, blend_func=('src_alpha', 'one_minus_src_alpha'))
--- src/visualisation_drawing/mc_tree_window.py
+++ src/visualisation_drawing/mc_tree_window.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton
 from vispy import app as VispyApp
 
@@ -7,21 +8,13 @@ from src.visualisation_drawing.mc_tree_canvas import MonteCarloTreeCanvas
 class MonteCarloTreeWindow(QMainWindow):
     def __init__(self, canvas: MonteCarloTreeCanvas):
         super().__init__()
-        self.mouse_tics = 0
         self.canvas = canvas
         self._setup_window(canvas)
 
     def show(self):
         super().show()
         VispyApp.run()
-
-    def wheelEvent(self, event):
-        self.mouse_tics = self.mouse_tics + event.angleDelta().y() / 120
-        if self.mouse_tics > 30:
-            self.mouse_tics = 30
-        elif self.mouse_tics < -30:
-            self.mouse_tics = -30
-        self.canvas.react_to_mouse_scroll(self.mouse_tics)
+        self.setFocus()
 
     def _setup_window(self, canvas):
         widget = QWidget()
