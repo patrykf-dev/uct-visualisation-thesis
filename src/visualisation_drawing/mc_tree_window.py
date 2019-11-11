5cacb9876315fddb55a5513b05ed400d31c795ee
gsrmeen <patryk.gsrmeen@gmail.com>
2019-11-11 15:40:54 +0100

    Add labels to preview window

--- src/visualisation_drawing/mc_tree_window.py
+++ src/visualisation_drawing/mc_tree_window.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QGridLayout, QLabel
 from vispy import app as VispyApp
 
 from src.visualisation_drawing.mc_tree_canvas import MonteCarloTreeCanvas
@@ -17,8 +17,27 @@ class MonteCarloTreeWindow(QMainWindow):
         self.setFocus()
 
     def _setup_window(self, canvas):
-        widget = QWidget()
-        self.setCentralWidget(widget)
-        widget.setLayout(QVBoxLayout())
-        widget.layout().addWidget(canvas.native)
-        widget.layout().addWidget(QPushButton())
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
+        main_layout = QHBoxLayout()
+        main_widget.setLayout(main_layout)
+        main_layout.addWidget(canvas.native)
+        right_panel_widget = QWidget()
+        right_panel_layout = QGridLayout()
+        self._fill_right_panel(right_panel_layout)
+        right_panel_widget.setLayout(right_panel_layout)
+        main_layout.addWidget(right_panel_widget)
+
+    @staticmethod
+    def _fill_right_panel(right_panel_layout):
+        labels = ["Id", "State name", "Move name", "Visits count", "Visits count pre", "Win score", "Average prize"]
+        bold_font = QtGui.QFont()
+        bold_font.setBold(True)
+        for i in range(len(labels)):
+            label_title = QLabel()
+            label_title.setText(labels[i] + ": ")
+            label_title.setFont(bold_font)
+            label_content = QLabel()
+            label_content.setText("_EMPTY_")
+            right_panel_layout.addWidget(label_title, i, 0)
+            right_panel_layout.addWidget(label_content, i, 1)
