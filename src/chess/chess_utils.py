9295febfc39a9a575554562769d170e21945eb2c
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2019-12-04 16:19:44 +0100

    Get rid of utilities class, move PastMove to chess_utils

--- src/chess/chess_utils.py
+++ src/chess/chess_utils.py
@@ -5,6 +5,18 @@ from src.chess.enums import GameStatus
 from src.chess.figures import *
 
 
+class PastMove:
+    def __init__(self, position_to, was_check, figure_moved, was_capture, position_from):
+        self.position_to = position_to
+        self.was_check = was_check
+        self.figure_moved = figure_moved
+        self.was_capture = was_capture
+        self.position_from = position_from
+
+    def __str__(self):
+        return f'Move {self.figure_moved} to: {self.position_to}, check: {self.was_check}, capture: {self.was_capture}'
+
+
 def do_pawn_double_move(board: Chessboard, move: ChessMove, figure_moved):
     figure_moved.set_can_be_captured_en_passant(True)
     # figure_moved.move(move.position_to)
