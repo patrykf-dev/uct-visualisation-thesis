e75a3bed7c3cc2ba0df61acf582e35659b631fb3
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2019-12-18 23:46:58 +0100

    Delete files that are no longer important

--- src/tictactoe/ttt_move.py
+++
@@ -1,8 +0,0 @@
-from src.uct.game.base_game_move import BaseGameMove
-
-
-class TicTacToeMove(BaseGameMove):
-    def __init__(self, x, y):
-        super().__init__()
-        self.x = x
-        self.y = y
