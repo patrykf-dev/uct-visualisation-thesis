9b59ef5e487ca852fd41a9fa8ad25290225c5f73
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-01-04 18:54:20 +0100

    Add docstrgins to chess_game_manager.py

--- src/chess/chess_game_manager.py
+++ src/chess/chess_game_manager.py
@@ -7,6 +7,9 @@ from src.chess.figures import Figure
 
 
 class ChessGameManager:
+    """
+    This class has information about board logic and its GUI. It handles clicks on a tile: selects/deselects figure.
+    """
     def __init__(self, width, height):
         self.board = Chessboard()
         self.if_figure_selected = False
@@ -17,12 +20,25 @@ class ChessGameManager:
         self.board.notify_tile_marked += self.on_tile_marked
 
     def on_tile_marked(self, sender, args):
+        """
+        Marks the appropriate tile after check is detected always after the move, to mark moved piece.
+        :param sender: information about object that sends the notification
+        :param args: handler additional arguments, in this case TileMarkArgs object
+        :return: None
+        """
         if args.tile_mark_type == TileMarkType.CHECKED:
             self.board_gui.mark_tile_checked(args.pos)
         elif args.tile_mark_type == TileMarkType.MOVED:
             self.board_gui.mark_tile_moved(args.pos)
 
     def react_to_tile_click(self, grid_pos):
+        """
+        Reacts when user clicks at chess GUI:
+        Selects the clicked figure or deselects it. When figure was already chosen and a possible move was clicked,
+        the move is executed.
+        :param grid_pos: tuple indicating a tile
+        :return: tuple of 2 values: bool if a move was made, a move itself (ChessMove object)
+        """
         if not self.if_figure_selected:
             self.select_figure(grid_pos)
             return False, None
@@ -43,6 +59,12 @@ class ChessGameManager:
                 return False, None
 
     def select_figure(self, grid_pos):
+        """
+        Selects figure and remembers the possible moves of it. It selects only, when a tile with figure was chosen and
+        its color matches the current moving player. Otherwise, the currently selected figure is deselected.
+        :param grid_pos: tuple indicating a tile
+        :return: None
+        """
         figure = Figure.get_figure(self.board.figures, grid_pos)
         if figure and figure.color == self.board.current_player_color:
             self.board_gui.mark_tile_selected(grid_pos)
@@ -55,6 +77,10 @@ class ChessGameManager:
             self.deselect_figure()
 
     def deselect_last_moved(self):
+        """
+        Deselects move that was marked as executed previously.
+        :return: None
+        """
         if not self.board.past_moves:
             return
         tile_1 = self.board.past_moves[-1].position_to
@@ -63,16 +89,28 @@ class ChessGameManager:
         self.board_gui.mark_tile_deselected(tile_2)
 
     def deselect_figure(self):
+        """
+        Deselects figure - make its tile unmarked. If it was king that got checked, the check mark is kept.
+        :return: None
+        """
         if self.selected_tile:
             is_king_selected = ChessUtils.is_king_selected_to_move_in_check(self.board, self.selected_tile)
             self.board_gui.mark_tile_deselected(self.selected_tile, when_checked=is_king_selected)
         self.reset_selected_tile()
 
     def reset_selected_tile(self):
+        """
+        After deselecting figure this function clears the possible moves list.
+        :return: None
+        """
         self.selected_tile = None
         self.board.possible_moves = []
         self.if_figure_selected = False
 
     def deselect_king(self):
+        """
+        Deselects king after running away from check.
+        :return: None
+        """
         if self.board.check:
             self.board_gui.mark_tile_deselected(self.board.figures.get_king_position(self.board.current_player_color))
