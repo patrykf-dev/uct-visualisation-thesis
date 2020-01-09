c9d89e0b13f092f73351af6d86124b43d827f9c4
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-01-09 23:00:44 +0100

    Add docstrings to mc_node_vis_details.py

--- src/visualisation_algorithm/mc_node_vis_details.py
+++ src/visualisation_algorithm/mc_node_vis_details.py
@@ -1,4 +1,8 @@
 class MonteCarloNodeVisualisationDetails:
+    """
+    Class stores information about MonteCarloTreeNode's position on canvas. The fields are calculated and used by
+    Improved Walker's Algorithm.
+    """
     def __init__(self, node, depth=0):
         self.x = -1.0
         self.y = depth
