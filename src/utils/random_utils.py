e23328f889a9013830519d8adea0e39f3f4d6e3c
Grzegorz_Kacprowicz <kacprowiczg@student.mini.pw.edu.pl>
2020-01-09 22:15:04 +0100

    Add docstring to random_utils.py

--- src/utils/random_utils.py
+++ src/utils/random_utils.py
@@ -4,5 +4,10 @@ random.seed(a=123456)
 
 
 def get_random_int(value_from, value_to):
+    """
+    :param value_from: int
+    :param value_to: int
+    :return: random int from range [value_from, value_to]
+    """
     span = value_to - value_from
     return int(span * random.random() + value_from)
