db9b3992807f07d65ce45b2942db96559fce4f09
gsrmeen <patryk.gsrmeen@gmail.com>
2020-01-13 19:47:29 +0100

    Refactor shader

--- src/main/resources/base/fragment_shader_vertices.c
+++ src/main/resources/base/fragment_shader_vertices.c
@@ -6,41 +6,32 @@ varying float v_radius;
 varying float v_linewidth;
 varying float v_antialias;
 
-float marker(vec2 P, float size);
+float calc(vec2 P, float size);
 
 void main()
 {
     float size = v_radius + 2 * (v_linewidth + v_antialias);
     float t = v_linewidth / 2.0 - v_antialias;
-
-    // The marker function needs to be linked with this shader
-    float r = marker(gl_PointCoord, size);
+    float r = calc(gl_PointCoord, size);
 
     float d = abs(r) - t;
     float r_max = (v_linewidth / 2.0 + v_antialias);
-    if(r > r_max)
-    {
-        // Outside circle
+    if(r > r_max) // Outside circle
         discard;
-    }
-    else if( d < 0.0 )
-    {
-        // Border
+    else if(d < 0.0) // Border
        gl_FragColor = v_fg_color;
-    }
-    else
+    else // Inner part
     {
-        // Inner part
-        float alpha = d/v_antialias;
-        alpha = exp(-alpha*alpha);
-        if (r > 0)
-            gl_FragColor = vec4(v_fg_color.rgb, alpha * v_fg_color.a);
-        else
+        float alpha = d / v_antialias;
+        alpha = exp(-alpha * alpha);
+        if (r <= 0)
             gl_FragColor = mix(v_bg_color, v_fg_color, alpha);
+        else
+            gl_FragColor = vec4(v_fg_color.rgb, alpha * v_fg_color.a);
     }
 }
 
-float marker(vec2 P, float size)
+float calc(vec2 P, float size)
 {
     vec2 someVector = (P.xy - vec2(0.5, 0.5));
     float r = length(someVector) * size;
