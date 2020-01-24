2371c1fc3c5d38a0abd9825ed13c5c1d34754811
gsrmeen <patryk.gsrmeen@gmail.com>
2020-01-25 00:03:44 +0100

    Fix paths loading

--- src/main/python/main_application/resources_container.py
+++ src/main/python/main_application/resources_container.py
@@ -5,7 +5,9 @@ class ResourcesContainer:
     inst = None
 
     def __init__(self, cntxt):
+        self.app_path = cntxt.get_resource()
         self.base_path = os.path.join(cntxt.get_resource(), "resources")
+        self.trees_path = os.path.join(cntxt.get_resource(), "sample-trees")
         ResourcesContainer.inst = self
 
     def get_resource_path(self, name):
